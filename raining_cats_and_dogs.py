8fab1a6ccd2d1dbd9626c35cfbf547c0421fa051
Fatimatou Ibrahim <fatiyaro793@gmail.com>
2025-07-20 15:24:28 -0400

    Add files via upload
    Add Module 1 Python script and screenshot

---
+++ raining_cats_and_dogs.py
@@ -0,0 +1,11 @@
+# This program will print: "It is raining cats and dogs."
+
+# Set the variables
+c = "cats"     # Represents the word 'cats'
+d = "dogs"     # Represents the word 'dogs'
+
+# Create the full sentence using string concatenation
+s = "It is raining " + c + " and " + d + "."
+
+# Display the sentence
+print(s)
